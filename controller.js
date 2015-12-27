12fa455cca0a32010d8cc1add6a98ad8cd444977
atf1999 <torth212@gmail.com>
2015-12-27 12:59:43 -0500

    Added comments

--- controller.js
+++ controller.js
@@ -2,10 +2,18 @@ var postApp = angular.module("postApp",[])
 postApp.controller('postController', function($scope){
   $scope.posts = [];
   $scope.addPost = function(){
-     $scope.posts.push({name: $scope.content, vote: 0});
-     $scope.content = "";
+     $scope.posts.push({name: $scope.content, vote: 0, comments: []});
+     $scope.content = ""; 
   };
   $scope.incrementVotes = function(post){
      post.vote += 1;
+  };
+  $scope.addComment = function(post){
+     console.log(post.name)
+     post.comments.push(
+     {
+      text: post.commentors
+     });
+     post.commentors= "";
   };
 });
